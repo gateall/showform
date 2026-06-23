7068cd4c4c2771d37b46eec9f3386553b992f56f
YOU IN YOUN <gateall@gmail.com>
2026-06-23 12:00:33 +0900

    Complete ShowForm Stage 04 inquiry management

--- adm/landing/inquiry_delete.php
+++ adm/landing/inquiry_delete.php
@@ -1,10 +1,14 @@
-﻿<?php
-$sub_menu = "990200";
-include_once('../../common.php');
-include_once(G5_ADMIN_PATH.'/admin.lib.php');
-if (!$is_admin) alert('관리자만 접근 가능합니다.', G5_URL);
+<?php
+include_once('./_common.php');
+
+auth_check_menu($auth, '900200', 'd');
+
 $id = isset($_GET['id']) ? (int)$_GET['id'] : 0;
-if ($id) {
-    sql_query(" delete from " . G5_TABLE_PREFIX . "landing_inquiry where id = '{$id}' ");
+if (!$id) {
+    alert('삭제할 문의가 없습니다.', './inquiry_list.php');
 }
-alert('삭제되었습니다.', './inquiry_list.php');
+
+$table = G5_TABLE_PREFIX . 'landing_inquiries';
+sql_query(" delete from {$table} where id = '{$id}' ");
+
+alert('문의가 삭제되었습니다.', './inquiry_list.php');
